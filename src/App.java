ebc196826ac950d88e97c23bc92d7c502d07b149
Axel Sjöholm <Axel.sjoholm@elev.ga.ntig.se>
2023-09-25 09:12:00 +0200

    B Delen

--- src/App.java
+++ src/App.java
@@ -13,5 +13,18 @@ public class App {
         System.out.println(Array1.length);
         double medelvärde1 = (Array1[0]+Array1[1]+Array1[2]+Array1[3]+Array1[4])/Array1.length;
         System.out.println("Medelvärded av array1 är "+medelvärde1);
+        int[] Array2 = new int[5];
+
+        Array2[0] = 18;
+        Array2[1] = 14;
+        Array2[2] = 16;
+        Array2[3] = 13;
+        Array2[4] = 19;
+
+        int summa12 = Array1[0]+Array1[1]+Array1[2]+Array1[3]+Array1[4]+Array2[0]+Array2[1]+Array2[2]+Array2[3]+Array2[4];
+        System.out.println("Summan av alla tal i array1 och array2 blir "+summa12);
+        double medelvärde2 = (Array2[0]+Array2[1]+Array2[2]+Array2[3]+Array2[4])/Array2.length;
+        System.out.println("Medelvärded av array2 är "+medelvärde2);
+
     }
 }
